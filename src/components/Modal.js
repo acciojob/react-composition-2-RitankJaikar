cec3ec9c6047c1ed05933e46dbb9a94fe0a2944e
RitankJaikar <ritank.jaikar@gmail.com>
2025-01-05 11:15:40 +0530

    Submit

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,7 +4,7 @@ export default function Modal({setShow}) {
     return (
         <div className="model-overlay model" onClick={() => setShow(false)}>
             <button className="model-close" onClick={() => setShow(false)}>Close</button>
-            <p className="model-p">This is content of the modal.</p>
+            <p className="model-p">This is the content of the modal.</p>
         </div>
     )
 }
